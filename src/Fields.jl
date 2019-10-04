06adb005cd4be9d9675c880d4176c9ec1af950b9
Rodney Polkinghorne <rpolkinghorne@swin.edu.au>
2019-10-04 15:23:21 +1000

    merge Grid type into Fields module

--- src/Fields.jl
+++ src/Fields.jl
@@ -1,5 +1,15 @@
 module Fields
 
-greet() = print("Hello World!")
+export Grid
+
+"""
+Rectangular domains with given step sizes and zero boundary conditions.
+In the future, the boundary conditions will be configurable.
+"""
+struct Grid
+    steps::Vector{Float64}
+    sizes::Vector{Int}
+    Grid(h,nx,l,ny) = new([h, l], [nx, ny])
+end
 
 end # module
